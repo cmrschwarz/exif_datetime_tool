f085442f51090de7469396f19d2b46be58ab6fcd
Christian Schwarz <cmrs@cmrs.io>
2022-05-01 19:25:39 +0200

    handle nested directories

--- exif_datetime_tool.py
+++ exif_datetime_tool.py
@@ -5,7 +5,6 @@ import datetime
 import re
 import sys
 import os
-import glob
 from typing import Optional
 
 
@@ -17,8 +16,11 @@ date_time_source_tags = [
 ]
 # we set the gathered information on all three of these,
 # because different tools use different things ...
-date_time_target_tags = date_time_source_tags
-
+date_time_target_tags = [
+    EXIF_TAG_STRINGS["DateTime"],
+    EXIF_TAG_STRINGS["DateTimeOriginal"],
+    EXIF_TAG_STRINGS["DateTimeDigitized"],
+]
 
 dir = os.path.realpath(os.path.dirname(__file__))
 input_path = os.path.join(dir, "input")
@@ -45,12 +47,13 @@ def try_get_datetime_from_filename(img) -> Optional[str]:
     return None
 
 
-for img in glob.glob(input_path + "/**.jp*"):
+def handle_image(dir_rel, img_name):
+    img_path = os.path.join(input_path, dir_rel, img_name)
     try:
-        img_pil = Image.open(img)
+        img_pil = Image.open(img_path)
     except (OSError, UnidentifiedImageError) as ex:
-        sys.stderr.write(f"{img}: failed to open image: {str(ex)}\n")
-        continue
+        sys.stderr.write(f"{img_path}: failed to open file: {str(ex)}\n")
+        return
     try:
         img_exif = img_pil.getexif()
         for dtst in date_time_source_tags:
@@ -58,21 +61,38 @@ for img in glob.glob(input_path + "/**.jp*"):
             if dt is not None:
                 break
         if dt is None:
-            dt = try_get_datetime_from_filename(img)
+            dt = try_get_datetime_from_filename(img_path)
             if dt is None:
-                print(f"{img}: failed to parse datetime from filename")
-                continue
-            print(f"{img}: interpreting filename date time as {dt}")
+                print(f"{img_path}: failed to parse datetime from filename")
+                return
+            print(f"{img_path}: interpreting filename date time as {dt}")
         else:
-            print(f"{img}: keeping existing time value {dt}")
+            print(f"{img_path}: keeping existing time value {dt}")
         for dttt in date_time_target_tags:
             if dttt not in img_exif:
                 img_exif[dttt] = dt
         img_pil.save(
-            os.path.join(output_path, os.path.basename(img)),
+            os.path.join(output_path, dir_rel, img_name),
             exif=img_exif
         )
     except (OSError, KeyError, ValueError) as ex:
-        sys.stderr.write(f"failed to handle image: {img}: {str(ex)}\n")
+        sys.stderr.write(f" {img_path}: failed to handle image: {str(ex)}\n")
     finally:
         img_pil.close()
+
+
+def handle_dir(dir_rel):
+    os.makedirs(os.path.join(output_path, dir_rel), exist_ok=True)
+    for obj in os.scandir(os.path.join(input_path, dir_rel)):
+        if obj.is_file():
+            if obj.name == ".gitignore":
+                continue
+            handle_image(dir_rel, obj.name)
+            continue
+        path = os.path.join(dir_rel, obj.name)
+        if obj.is_dir:
+            handle_dir(path)
+        elif obj.is_symlink:
+            sys.stderr.write(f"{path}: ignoring symlink\n")
+
+handle_dir("")
